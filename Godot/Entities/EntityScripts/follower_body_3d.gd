1cdb90d5139a8b0a3a03a84b52938b7a9c4cbb01
shquam <87450533+shquam@users.noreply.github.com>
2025-10-12 04:23:09 -0400

    add jog animation

--- Godot/Entities/EntityScripts/follower_body_3d.gd
+++ Godot/Entities/EntityScripts/follower_body_3d.gd
@@ -9,6 +9,7 @@ class_name FollowerBody3D
 var player_position: Vector3
 var sprint_distance_squared: float = 35.
 var blend_speed: float = 8.
+var is_sprinting: bool = false
 
 func _ready() -> void:
 	if(navigation_agent):
@@ -30,9 +31,11 @@ func _physics_process(delta: float) -> void:
 		return
 	
 	var run_scale: float = 1
+	is_sprinting = false
 	print(global_position.distance_squared_to(player_position))
 	if(global_position.distance_squared_to(player_position) > sprint_distance_squared):
 		run_scale = 1.7
+		is_sprinting = true
 	
 	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
 	var new_velocity: Vector3 = global_position.direction_to(next_path_position) * movement_speed * run_scale
@@ -54,17 +57,27 @@ func animate_npc(delta: float) -> void:
 	assert(model, "Model not instantiated!")
 	assert(animation_tree, "Animation Tree not instantiated!")
 	if velocity == Vector3.ZERO:
-		change_blend_position(delta, 0)
+		change_blend_position(delta, 0, 0)
+		return
+	
+	## If velocity is not 0
+	var npc_to_player: Vector3 = global_position.direction_to(player_position)
+	var npc_to_movement_direction: Vector3 = global_position.direction_to(global_position+velocity)
+	
+	model.look_at(player_position, Vector3.UP, true)
+	if npc_to_player.angle_to(npc_to_movement_direction) < PI/2:
+		# Moves forward
+		if is_sprinting:
+			change_blend_position(delta, 0, 1)
+		change_blend_position(delta, 1, 0)
 	else:
-		model.look_at(player_position, Vector3.UP, true)
-		var npc_to_player: Vector3 = global_position.direction_to(player_position)
-		var npc_to_movement_direction: Vector3 = global_position.direction_to(global_position+velocity)
-		if npc_to_player.angle_to(npc_to_movement_direction) < PI/2:
-			change_blend_position(delta, 1)
-		else:
-			change_blend_position(delta, -1)
+		# Moves backward
+		change_blend_position(delta, -1, 0)
 
-func change_blend_position(delta: float, blend_to: float) -> void:
-	var blend_position: float = animation_tree["parameters/BlendSpace1D/blend_position"]
-	blend_position = lerpf(blend_position, blend_to, blend_speed * delta)
-	animation_tree["parameters/BlendSpace1D/blend_position"] = blend_position
+func change_blend_position(delta: float, blend_x_to: float, blend_y_to: float) -> void:
+	var blend_position_x: float = animation_tree["parameters/BlendSpace2D/blend_position"].x
+	var blend_position_y: float = animation_tree["parameters/BlendSpace2D/blend_position"].y
+	blend_position_x = lerpf(blend_position_x, blend_x_to, blend_speed * delta)
+	blend_position_y = lerpf(blend_position_y, blend_y_to, blend_speed * delta)
+	animation_tree["parameters/BlendSpace2D/blend_position"].x = blend_position_x
+	animation_tree["parameters/BlendSpace2D/blend_position"].y = blend_position_y
