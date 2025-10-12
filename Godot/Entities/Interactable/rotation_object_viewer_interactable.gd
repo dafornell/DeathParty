768b3bcbb7ac4c710c765ebf41cb7b22a2b3a75b
green-yellw <jack-g@sky.com>
2025-10-12 12:58:11 +0100

    remove `self`s from obj viewer rotate script
    if we dont specify an object, these things just get set/called on the object the script is attached to, so the `self`s arent needed here 🤓
    
    (theyre fine tho, we can revert this if ppl find it more readable with them)

--- Godot/Entities/Interactable/rotation_object_viewer_interactable.gd
+++ Godot/Entities/Interactable/rotation_object_viewer_interactable.gd
@@ -8,14 +8,14 @@ const START_ROTATION = Vector3(deg_to_rad(90) - START_OFFSET, deg_to_rad(180) + 
 
 func _ready() -> void:
 	print("Object viewer rotatatble")
-	self.rotation = START_ROTATION
+	rotation = START_ROTATION
 	Interact.mouse_position_changed.connect(on_mouse_pos_changed)
 
 func on_mouse_pos_changed(delta : Vector2):
 	if !dragging:
 		return
-	self.rotate_x(delta.y * 0.005)
-	self.rotate_y(delta.x * 0.005)
+	rotate_x(delta.y * 0.005)
+	rotate_y(delta.x * 0.005)
 
 ##INHERITED
 func on_mouse_down() -> void:
