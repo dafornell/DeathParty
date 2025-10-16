af4c9eff9c1cd1fcb29fadaf30f464824f5031b8
green-yellw <jack-g@sky.com>
2025-10-16 15:57:49 +0100

    add toggleable quest markers in npc popups

--- Godot/Common/Popup UI/npc_interact_popup.gd
+++ Godot/Common/Popup UI/npc_interact_popup.gd
@@ -2,10 +2,43 @@
 extends Node3D
 
 
+@export var quest_marker_enabled := false:
+	set(value):
+		quest_marker_enabled = value
+
+		# i think export vars get set on enter_tree so we have to check this
+		# isnt null in case it tries to hide this before it exists and causes
+		# an error ↓
+
+		# TODO: maybe make this script just send a signal down and have the
+		#		quest marker just show/hide itself to avoid any weird stuff
+		#		like this
+		#				- jack
+		if quest_marker == null:
+			return
+
+		if quest_marker_enabled:
+			quest_marker.show()
+			if not Engine.is_editor_hint():
+				show()
+		else:
+			quest_marker.hide()
+			if not Engine.is_editor_hint():
+				hide()
+
 @export var x_offset: float = -1.15
 @export var y_offset: float = 2
 
 @onready var parent_npc: NPC = get_parent()
+@onready var quest_marker: Sprite3D = %QuestMarker
+
+
+func _enter_tree() -> void:
+	Events.toggle_quest_marker.connect(_on_toggle_quest_marker)
+
+
+func _ready() -> void:
+	quest_marker_enabled = quest_marker_enabled
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,3 +48,20 @@ func _physics_process(_delta: float) -> void:
 	# keep its position consistent
 	if parent_npc:
 		global_position = parent_npc.global_position + Vector3(x_offset, y_offset, 0)
+
+
+# make the popup always show (regardless of player distance to npc) if the
+# quest marker is enabled by forcing it back to visible if it gets hidden
+func _on_visibility_changed() -> void:
+	if visible:
+		return
+
+	if quest_marker_enabled:
+		show()
+
+
+func _on_toggle_quest_marker(npc_name: String, value: bool) -> void:
+	if npc_name != parent_npc.name:
+		return
+
+	quest_marker_enabled = value
--- Godot/Singletons/events.gd
+++ Godot/Singletons/events.gd
@@ -2,3 +2,5 @@ extends Node
 
 
 signal dialogue_box_text_changed
+
+signal toggle_quest_marker(npc_name: String, value: bool)
