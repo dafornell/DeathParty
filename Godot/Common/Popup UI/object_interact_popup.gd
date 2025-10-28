ea4635fb0fe4e09855240dfd284ede6f24f58b1a
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-10-28 14:25:07 +0000

    add popup offset export vars

--- Godot/Common/Popup UI/object_interact_popup.gd
+++ Godot/Common/Popup UI/object_interact_popup.gd
@@ -1,6 +1,10 @@
 @tool
 extends Node3D
 
+@export var x_offset: float = -1
+@export var y_offset: float = 0.5
+
+@onready var parent: Interactable = get_parent()
 
 @export var prop_name: String = "prop name":
 	set(name):
@@ -11,3 +15,8 @@ extends Node3D
 	set(name):
 		action = name
 		%ActionLabel.text = action
+
+
+func _physics_process(_delta: float) -> void:
+	if parent != null and parent is Node3D:
+		global_position = parent.global_position + Vector3(x_offset, y_offset, 0)
