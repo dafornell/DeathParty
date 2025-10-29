5708d1998acb2f04ee278766c85ba8a587efd08f
green-yellw <jack-g@sky.com>
2025-10-29 17:05:53 +0000

    add sunday studios splash screen

--- Godot/Utilities/splash_screens.gd
+++ Godot/Utilities/splash_screens.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
 
+@onready var background: ColorRect = %Background
+@onready var sunday_logo: TextureRect = %SundayLogo
 @onready var fmod_logo: TextureRect = %FmodLogo
 
 
@@ -12,8 +14,17 @@ func _enter_tree() -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var tween: Tween = create_tween()
+	tween.tween_property(sunday_logo, "modulate:a", 1, 1)
+	tween.tween_interval(1)
+	tween.tween_property(sunday_logo, "modulate:a", 0, 1)
+
+	tween.tween_interval(0.5)
+
 	tween.tween_property(fmod_logo, "modulate:a", 1, 1)
 	tween.tween_interval(1)
 	tween.tween_property(fmod_logo, "modulate:a", 0, 1)
+
+	tween.tween_property(background, "color:a", 0, 1)
+
 	await tween.finished
 	queue_free()
