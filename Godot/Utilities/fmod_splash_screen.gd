9598b81f26f4bcd84ffe3986d3b2d9352aff3ba4
green-yellw <jack-g@sky.com>
2025-10-29 12:00:23 +0000

    add fmod splash screen

---
+++ Godot/Utilities/fmod_splash_screen.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+
+@onready var fmod_logo: TextureRect = %FmodLogo
+
+
+func _enter_tree() -> void:
+	if not OS.has_feature("editor"):
+		show()
+	else:
+		queue_free()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property(fmod_logo, "modulate:a", 1, 1)
+	tween.tween_interval(1)
+	tween.tween_property(fmod_logo, "modulate:a", 0, 1)
+	await tween.finished
+	queue_free()
