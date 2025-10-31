e0cc7db614ae676dd8f05660b54e5ae5a37ed82f
Aubrey Clark (ketexon) <zane.a.s.clark@gmail.com>
2025-10-31 12:27:39 -0700

    made intro state repeatedly disable input and prevented potential socklock by storing if intro sequence is finished instead of using event

--- Godot/Utilities/tutorial/states/tutorial_state_intro.gd
+++ Godot/Utilities/tutorial/states/tutorial_state_intro.gd
@@ -1,13 +1,24 @@
 extends TutorialState
 
+var _intro_finished := false
+
 func _ready() -> void:
-	Events.intro_finished.connect(transition)
+	Events.intro_finished.connect(_on_intro_finished)
+
+func _on_intro_finished() -> void:
+	_intro_finished = true
+	if active:
+		transition()
 
 func _process(_delta: float) -> void:
 	if Globals.player:
 		Globals.player.movement_disabled = true
-		process_mode = Node.PROCESS_MODE_DISABLED
-	
+
+func enter_state() -> void:
+	super()
+	if _intro_finished:
+		transition()
+
 func exit_state() -> void:
 	Globals.player.movement_disabled = false
 	super()
