c6753a9aa8f4dd27fd89dc63aa6e7d3b0f45a1c5
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-11-07 23:47:09 +0000

    implement main menu music
    very rough
    
    want to make transition smoother and implement the exception for the bedroom scene
    
    but basically seems to work

--- Godot/Utilities/title_screen.gd
+++ Godot/Utilities/title_screen.gd
@@ -2,10 +2,16 @@ extends CanvasLayer
 
 
 @export var title_image: TextureRect
+@onready var music: FmodEventEmitter3D = %Music
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GuiSystem.in_title_screen = true
+	#if ContentLoader.active_scene == null:
+		#await ContentLoader.finished_loading
+		#if ContentLoader.active_scene.name != "Bedroom":
+			#FmodServer.set_global_parameter_by_name("InTitleScreen", 0)
 
 
 func _on_settings_button_pressed() -> void:
