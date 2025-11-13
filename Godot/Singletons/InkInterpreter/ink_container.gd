911521948e7162d2c031fd81b79dc9a7838fdd78
Aubrey Clark (ketexon) <zane.a.s.clark@gmail.com>
2025-11-12 19:05:00 -0800

    added tags to ink parser

--- Godot/Singletons/InkInterpreter/ink_container.gd
+++ Godot/Singletons/InkInterpreter/ink_container.gd
@@ -13,35 +13,35 @@ var redirects : Dictionary[String, InkContainer]
 var visits : int = 0
 
 func _init(
-    _parent_container: InkContainer,
-    _name : String,
-    _path : String, 
-    _evaluation_stack: Array[String] = [], 
-    is_redirect : bool = false,
+	_parent_container: InkContainer,
+	_name : String,
+	_path : String, 
+	_evaluation_stack: Array[String] = [], 
+	is_redirect : bool = false,
 ) -> void:
-    #print("Initial container names: ", _name)
-    super(_parent_container, _path, _evaluation_stack)
-    name = _name
-    if parent_container:
-        if is_redirect:
-            parent_container.redirects[self.name] = self
-        else:
-            parent_container.dialogue_lines.push_back(self)
+	#print("Initial container names: ", _name)
+	super(_parent_container, _path, _evaluation_stack)
+	name = _name
+	if parent_container:
+		if is_redirect:
+			parent_container.redirects[self.name] = self
+		else:
+			parent_container.dialogue_lines.push_back(self)
 
 func tostring() -> String:
-    var export_str : String = "------CONTAINER " + name + " at " + path + ":\n"
-    export_str = export_str + "---"+name+" Dialogue Lines: "
-    if dialogue_lines.is_empty():
-        export_str = export_str + "[]"
-    export_str = export_str + "\n"
-    for line in dialogue_lines:
-        if line is InkLineInfo:
-        #print("Parsing json: ", line, " ", line.path,   " container=", line is InkContainer, " redi    rect=", line is InkRedirect, " line=", line is InkLineInfo)
-            export_str = export_str + line.tostring() + "\n"
-    export_str = export_str + "---"+name+" Choices: "
-    if dialogue_choices.is_empty():
-        export_str = export_str + "[]"
-    export_str = export_str + "\n"
-    for choice : InkChoiceInfo in dialogue_choices:
-        export_str = export_str + choice.tostring()
-    return export_str
+	var export_str : String = "------CONTAINER " + name + " at " + path + ":\n"
+	export_str = export_str + "---"+name+" Dialogue Lines: "
+	if dialogue_lines.is_empty():
+		export_str = export_str + "[]"
+	export_str = export_str + "\n"
+	for line in dialogue_lines:
+		if line is InkLineInfo:
+		#print("Parsing json: ", line, " ", line.path,   " container=", line is InkContainer, " redi    rect=", line is InkRedirect, " line=", line is InkLineInfo)
+			export_str = export_str + line.tostring() + "\n"
+	export_str = export_str + "---"+name+" Choices: "
+	if dialogue_choices.is_empty():
+		export_str = export_str + "[]"
+	export_str = export_str + "\n"
+	for choice : InkChoiceInfo in dialogue_choices:
+		export_str = export_str + choice.tostring()
+	return export_str
