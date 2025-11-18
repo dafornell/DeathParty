6442d0b235ada36fa69b48465066f11d48fe9666
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-11-18 21:04:52 +0000

    add test vo files and fmod dialogue player
    completely untested but in theory this should be hooked up and will play audio files when dialogue lines with a valid ID are shown on screen

--- Godot/Singletons/VoiceLinePlayer/voice_line_player.gd
+++ Godot/Singletons/VoiceLinePlayer/voice_line_player.gd
@@ -1,12 +1,16 @@
 extends Node
 
+
 func _ready() -> void:
 	Events.dialogue_line_displayed.connect(_on_dialogue_line_displayed)
+
 
 func _on_dialogue_line_displayed(line: InkLineInfo) -> void:
 	if "id" in line.metadata:
 		_play_voice_line(line.metadata["id"])
 
-#TODO: Play the sound using FMOD
+
 func _play_voice_line(id: String) -> void:
-	pass
+	var event_instance = FmodServer.create_event_instance("event:/500_DX/VoiceLinePlayer")
+	event_instance.set_programmer_callback(id)
+	event_instance.start()
