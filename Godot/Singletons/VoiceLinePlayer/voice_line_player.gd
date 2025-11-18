1c307bf8d33482b79fd6991b161d65d92b6c57c1
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-11-18 21:09:48 +0000

    adjust voice line player behaviour
    in theory should stop voice lines from playing over each other by not creating a new instance of the event each time

--- Godot/Singletons/VoiceLinePlayer/voice_line_player.gd
+++ Godot/Singletons/VoiceLinePlayer/voice_line_player.gd
@@ -1,8 +1,11 @@
 extends Node
 
+var event_instance: FmodEvent
+
 
 func _ready() -> void:
 	Events.dialogue_line_displayed.connect(_on_dialogue_line_displayed)
+	event_instance = FmodServer.create_event_instance("event:/500_DX/VoiceLinePlayer")
 
 
 func _on_dialogue_line_displayed(line: InkLineInfo) -> void:
@@ -11,6 +14,5 @@ func _on_dialogue_line_displayed(line: InkLineInfo) -> void:
 
 
 func _play_voice_line(id: String) -> void:
-	var event_instance = FmodServer.create_event_instance("event:/500_DX/VoiceLinePlayer")
 	event_instance.set_programmer_callback(id)
 	event_instance.start()
