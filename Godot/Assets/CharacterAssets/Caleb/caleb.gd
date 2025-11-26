f0f676de2762ffa71437c0c63c95a7877475d4ee
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-11-26 00:03:49 +0000

    fix logic for changing caleb location

--- Godot/Assets/CharacterAssets/Caleb/caleb.gd
+++ Godot/Assets/CharacterAssets/Caleb/caleb.gd
@@ -9,9 +9,6 @@ func _enter_tree() -> void:
 
 
 func _on_entrance_convo_finished() -> void:
-	character_resource.character_location = Globals.SCENES.Basement
-	SaveSystem.save_to_disk()
-
 	if kitchen_door != null:
 		animation_player.play("Skateboarding")
 		# offset slightly from kitchen door so he doesnt skate through the girls standing in front of it
@@ -21,6 +18,7 @@ func _on_entrance_convo_finished() -> void:
 		look_at(target_position, Vector3.UP, true)
 		var t: Tween = create_tween()
 		t.tween_property(self, "global_position", target_position, 3.5)
-		await t.finished
+		await get_tree().create_timer(2).timeout
 
-	fade_away_then_delete()
+	character_resource.change_location("Basement")
+	SaveSystem.save_to_disk()
